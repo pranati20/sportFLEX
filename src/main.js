62be16634d26d4c6b9a1c5e65cbfca2b3c1811f4
Pranati Sattarapu <pranatisattarapu@gmail.com>
2024-03-26 16:01:27 +0530

    Updated dependencies and switched to bcryptjs for password hashing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const connectToDatabase = require("./teamsConfig");
